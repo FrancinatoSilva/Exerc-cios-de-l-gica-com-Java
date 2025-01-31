ee2ce63a43d014910682aec23225ff540732d126
Francinato Silva <167146805+FrancinatoSilva@users.noreply.github.com>
2025-01-31 13:29:23 -0300

    Delete ex7_22.java

--- ex7_22.java
+++
@@ -1,9 +0,0 @@
-package estiloFerKipper;
-
-//Faça um algoritmo que leia dois valores booleanos (lógicos) e determine se ambos são VERDADEIRO ou FALSO.
-public class ex7_22 {
-    public static void main(String[] args) {
-
-
-    }
-}
